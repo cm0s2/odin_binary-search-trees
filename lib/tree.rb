3dc05747de551b5d5342086a6ae23d321f3f4b73
Carl Gudmundsson <komoss@gmail.com>
2022-08-23 21:14:56 +0200

    Implement build_tree, inorder, preorder and postorder

---
+++ lib/tree.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require_relative "node"
+
+class Tree
+  def initialize(array)
+    @root = build_tree(array)
+  end
+
+  def build_tree(array)
+    return nil if array.empty?
+
+    # Sort array and remove duplicates
+    array_uniq_sorted = array.sort.uniq
+
+    # Find middle index
+    mid = array_uniq_sorted.length / 2
+
+    # Make the middle element the root
+    root = Node.new(array_uniq_sorted[mid])
+    root.left = build_tree(array_uniq_sorted[...mid])
+    root.right = build_tree(array_uniq_sorted[mid+1...])
+
+    return root
+  end
+
+  def inorder(node=@root)
+    return if node.nil?
+
+    preorder(node.left)
+    puts node.data
+    preorder(node.right)
+  end
+
+  def preorder(node=@root)
+    return if node.nil?
+
+    puts node.data
+    preorder(node.left)
+    preorder(node.right)
+  end
+
+  def postorder(node=@root)
+    return if node.nil?
+    
+    preorder(node.left)
+    preorder(node.right)
+    puts node.data
+  end
+end
